f384db91aa42e7e9850d47f8f5b3d454e32609e4
Hugo Lupo <hugo.lupo@epitech.eu>
2022-05-27 17:59:54 +0200

    front + connect to contract ok

---
+++ front/src/App.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { CONTRACT_ADRESS, TOKEN_FACTORY } from "./Globals"
+import { Contract, ethers } from "ethers";
+import { Token, Token__factory } from "./typechain-types";
+
+export const App: React.FC = () => {
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+
+  const checkWallet = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("pas metamask");
+      return;
+    } else console.log("metamask ok")
+
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      setCurrentAccount(account);
+      return;
+    } else console.log("No auth account found")
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum)
+      console.log("no metamask")
+
+    try {
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      console.log("Account found: " + accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) { console.log(err); }
+  }
+
+  const test = async () => {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const add = await signer.getAddress();
+      console.log("Signer = ", signer)
+      console.log("Add = ", add);
+      const myContract = new Contract(CONTRACT_ADRESS, TOKEN_FACTORY.interface, signer) as Token;
+
+      const res = await myContract.balanceOf(add);
+
+      console.log("Balance: ", res);
+    }
+  }
+
+  useEffect(() => {
+    console.log("yo")
+    checkWallet();
+  }, [])
+
+  return (
+    <div >
+      {currentAccount == null && <button onClick={() => connectWalletHandler()}><span>Connect</span></button>}
+      {currentAccount && <h1>Connected with address {currentAccount}</h1>}
+      <button onClick={() => test()}><span>Check balance</span></button>
+    </div>
+  )
+}
+
+export default App;
---
+++ front/src/Globals.ts
@@ -0,0 +1,4 @@
+import { Token__factory } from "./typechain-types";
+
+export const CONTRACT_ADRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+export const TOKEN_FACTORY = new Token__factory();
