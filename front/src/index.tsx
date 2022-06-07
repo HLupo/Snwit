3731a03e5b3b3a207703e8ae73de41e989d4ceaa
Hugo Lupo <hugo.lupo@epitech.eu>
2022-06-07 10:11:52 +0200

    Module resolution correction

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
