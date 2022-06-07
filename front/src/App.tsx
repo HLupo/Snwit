3b3632af2aab8efecb93e2851bea5e70383ee64f
Hugo Lupo <hugo.lupo@epitech.eu>
2022-06-07 09:29:10 +0200

    Transaction

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { CONTRACT_ADRESS, TOKEN_FACTORY } from "./Globals"
 import { Contract, ethers } from "ethers";
-import { Token, Token__factory } from "./typechain-types";
+import { Token } from "./typechain-types";
 
 export const App: React.FC = () => {
 
   const [currentAccount, setCurrentAccount] = useState(null);
+  const [sendAddress, setSendAddress] = useState("");
+  const [sendAmount, setAmount] = useState("");
 
 
   const checkWallet = async () => {
@@ -52,6 +54,26 @@ export const App: React.FC = () => {
       const res = await myContract.balanceOf(add);
 
       console.log("Balance: ", res);
+      const test = parseInt(res._hex);
+      console.log(test);
+    }
+  }
+
+  const sendToken = async () => {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const add = await signer.getAddress();
+      console.log("Signer = ", signer)
+      console.log("Add = ", add);
+      const myContract = new Contract(CONTRACT_ADRESS, TOKEN_FACTORY.interface, signer) as Token;
+
+      const res = await myContract.transfer(sendAddress, sendAmount);
+      const test = await res.wait();
+
+      console.log(test);
     }
   }
 
@@ -65,8 +87,17 @@ export const App: React.FC = () => {
       {currentAccount == null && <button onClick={() => connectWalletHandler()}><span>Connect</span></button>}
       {currentAccount && <h1>Connected with address {currentAccount}</h1>}
       <button onClick={() => test()}><span>Check balance</span></button>
+      <div style={{ display: "flex", flexDirection: "row", }}>
+        <span style={{ marginRight: 5 }}>Send MHT to :</span>
+        <input style={{}} onChange={(e) => setSendAddress(e.target.value)}></input>
+        <span style={{ marginLeft: 5 }}>Amount : </span>
+        <input style={{ marginLeft: 5 }} onChange={(e) => setAmount(e.target.value)}></input>
+        <button style={{ marginLeft: 5 }} onClick={() => sendToken()}>Send</button>
+      </div>
     </div>
   )
 }
 
 export default App;
+
+
